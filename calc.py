5d9d102845b24b693f06c85b2ff59e27868580d7
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2023-01-10 14:33:23 -0700

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,5 +1,17 @@
-numA2 = input("enter X2 ")
-numA1 = input("enter X1 ")
-numB2 = input("enter Y2 ")
-numB1 = input("enter Y1 ")
+import math
+# input
+numA2 = float(input("enter X2 "))
+numA1 = float(input("enter X1 "))
+numB2 = float(input("enter Y2 "))
+numB1 = float(input("enter Y1 "))
 
+# process
+NumA= numA2 - numA1
+NumB= numB2 - numB1
+if NumA or NumB < 0:
+ NumA or NumB* -1
+
+ AFNumA = NumA * NumA
+ AFNumB = NumB * NumB
+ NFNum = AFNumA + AFNumB
+ print("the distance between is " + str(math.sqrt(NFNum)))
